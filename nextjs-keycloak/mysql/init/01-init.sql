fe35fe605736da3315832f8b5660933090eece58
Tanut-M2 <tanat44@gmail.com>
2024-11-10 21:38:09 +0100

    working mysql with keycloak example

--- nextjs-keycloak/mysql/init/01-init.sql
+++ nextjs-keycloak/mysql/init/01-init.sql
@@ -1,11 +1,9 @@
-# create databases
-CREATE DATABASE IF NOT EXISTS `ndc`;
+# create database for tanut
+CREATE DATABASE IF NOT EXISTS `tanut`;
+CREATE USER 'tanut'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON tanut.* TO 'tanut'@'%';
+
+# create database for keycloak
 CREATE DATABASE IF NOT EXISTS `keycloak`;
-
-# create user for ndc
-CREATE USER 'ndc'@'localhost' IDENTIFIED BY 'password'
-GRANT ALL PRIVILEGES ON ndc.* TO 'ndc'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
-
-# create user for keycloak
-CREATE USER 'keycloak'@'localhost' IDENTIFIED BY 'password'
-GRANT ALL PRIVILEGES ON keycloak.* TO 'keycloak'@'%' IDENTIFIED BY 'PASSWORD' WITH GRANT OPTION;
+CREATE USER 'keycloak'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON keycloak.* TO 'keycloak'@'%';
