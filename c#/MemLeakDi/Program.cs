436598d9f8f863413aec54372d8c6cb7081847a3
Tanut <tanat44@gmail.com>
2025-03-17 11:57:00 +0900

    add memory hard limit

--- c#/MemLeakDi/Program.cs
+++ c#/MemLeakDi/Program.cs
@@ -14,18 +14,18 @@ builder.Services.AddSwaggerGen();
 //builder.Services.AddScoped<ServiceBScoped>();
 
 //// EXPERIMENT 2: Try to solve DI error by using Func. Result in bad mem leak because ServiceB will never goes out of scope. ServiceB.Dispose never runs.
-//builder.Services.AddHostedService<ServiceA2Hosted>();
-//builder.Services.AddScoped<ServiceBScoped>();
-//builder.Services.AddSingleton<Func<ServiceBScoped>>((instance) => () =>
-//{
-//    var scope = instance.CreateScope();
-//    return scope.ServiceProvider.GetService<ServiceBScoped>();
-//});
+builder.Services.AddHostedService<ServiceA2Hosted>();
+builder.Services.AddScoped<ServiceBScoped>();
+builder.Services.AddSingleton<Func<ServiceBScoped>>((instance) => () =>
+{
+    var scope = instance.CreateScope();
+    return scope.ServiceProvider.GetService<ServiceBScoped>();
+});
 
 //// EXPERIMENT 3: Try to fix error in Experiment1 without mem leak
 //// Ref https://learn.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-9.0
-builder.Services.AddHostedService<ServiceA3Hosted>();
-builder.Services.AddScoped<ServiceBScoped>();
+//builder.Services.AddHostedService<ServiceA3Hosted>();
+//builder.Services.AddScoped<ServiceBScoped>();
 
 var app = builder.Build();
 
