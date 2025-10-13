c0d0160af169a258baee7120a01d79c523c3a72e
tanat44 <tanat44@gmail.com>
2025-10-13 14:49:22 +0200

    working kafka node client

--- node/kafka-client/index.js
+++ node/kafka-client/index.js
@@ -1,18 +1,28 @@
-const { Kafka } = require('kafkajs')
+import { Kafka } from 'kafkajs'
 
 const kafka = new Kafka({
   clientId: 'my-app',
   brokers: ['kafka-broker-service:9093'],
+  ssl: false,
+  sasl: {
+    mechanism: 'plain', // scram-sha-256 or scram-sha-512
+    username: 'admin',
+    password: 'admin-secret'
+  },
 })
 
 const producer = kafka.producer()
 
 await producer.connect()
+console.log('Connected')
 await producer.send({
   topic: 'test-topic',
   messages: [
-    { value: 'Hello KafkaJS user!' },
+    {
+      value: `Hello KafkaJS user at ${new Date().toLocaleTimeString()}`,
+    },
   ],
 })
 
 await producer.disconnect()
+console.log('Disconnected')
