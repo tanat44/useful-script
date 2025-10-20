84827474535d631fc643476936598f6574918207
Tanut-M2 <tanat44@gmail.com>
2025-10-20 22:29:51 +0200

    delete wrong file

--- arduino/esp32-joystick-mqtt/frontend/src/event.ts
+++
@@ -1,3 +0,0 @@
-export const EVENT_ENCODER_RAW = "encoder.raw";
-export const EVENT_ENCODER_DATA = "encoder.data";
-export const EVENT_MOTOR_OSCILLATE = "motor.oscillate";
--- arduino/esp32-joystick-mqtt/frontend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import "./encoder";
-import "./main.css";
-import "./motor/autotune";
-import { testOscillation } from "./motor/autotune";
-import "./motor/position";
-import "./motor/pwm";
-import "./mqtt";
-
-document.getElementById("testOscillation").onclick = testOscillation;
