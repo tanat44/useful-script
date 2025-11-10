f79ff5c2b5c56f99921da2ed09510d50bfba5f8b
tanat44 <tanat44@gmail.com>
2025-11-10 10:08:48 +0100

    support run qr code with args

--- python/qr-reader-thread/main.py
+++ python/qr-reader-thread/main.py
@@ -1,10 +1,19 @@
+import argparse
 import time
 from queue import Queue
 from reader_worker import ReaderWorker
 
-WORKERS = 4
-JOBS = 20
+# parse args
+parser = argparse.ArgumentParser(
+                    prog='qr reader multi-thread',
+                    description='find optimal number of threads for qr code reading',)
+parser.add_argument('-w', '--workers', type=int, default=4, help='number of worker threads (default: 4)')
+parser.add_argument('-j', '--jobs', type=int, default=20, help='number of jobs (default: 20)')
+args = parser.parse_args()
+WORKERS = args.workers    
+JOBS = args.jobs
 
+# start processing
 start_time = time.time()
 count = [i for i in range(0, WORKERS)]
 workers = []
@@ -31,4 +40,4 @@ for worker in workers:
     worker.stop()
 
 end_time = time.time()
-print(f'it takes {end_time-start_time}')
+print(f'{WORKERS} workers process {JOBS} jobs: takes {end_time-start_time}s')
