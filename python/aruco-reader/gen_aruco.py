763057b1ebcd5bd1f21465cafea2546bfc3ad138
tanat44 <tanat44@gmail.com>
2025-11-27 10:58:10 +0100

    update

--- python/aruco-reader/gen_aruco.py
+++ python/aruco-reader/gen_aruco.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 
+MARKER_ID = 23
+
 dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_6X6_250)
-marker_image = cv.aruco.generateImageMarker(dictionary, 44, 200)
-cv.imwrite("marker44.png", marker_image)
+marker_image = cv.aruco.generateImageMarker(dictionary, MARKER_ID, 200)
+cv.imwrite(f'marker{MARKER_ID}.png', marker_image)
