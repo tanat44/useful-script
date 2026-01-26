7f1ae68975e0fa55928bdddbf330230bbe8d9125
Tanut-M2 <tanat44@gmail.com>
2026-01-26 17:32:38 +0700

    update promptpay

--- node/thaiqrpayment/crcgen.js
+++ node/thaiqrpayment/crcgen.js
@@ -4,14 +4,15 @@ const crc16 = require("crc/crc16ccitt");
 // original
 const asciiString =
   "00020101021129370016A000000677010111011300669245317755802TH54076000.00530376463049B3B";
-const encoder = new TextEncoder();
-const bytes = encoder.encode(asciiString);
-QRCode.toFile("output/qrpromptpay.png", [
-  { data: new Uint8ClampedArray(bytes), mode: "byte" },
-]);
 
 // try matching check crc16
 const withoutCrc =
   "00020101021129370016A000000677010111011300669245317755802TH54076000.0053037646304";
 const crcResult = crc16(withoutCrc).toString(16);
 console.log("crc result", crcResult);
+const promptPayCode = `${withoutCrc}${crcResult.toUpperCase()}`;
+const encoder = new TextEncoder();
+const bytes = encoder.encode(promptPayCode);
+QRCode.toFile("output/crcgen-test.png", [
+  { data: new Uint8ClampedArray(bytes), mode: "byte" },
+]);
--- node/thaiqrpayment/promptpaygen.js
+++ node/thaiqrpayment/promptpaygen.js
@@ -3,16 +3,35 @@ const crc16 = require("crc/crc16ccitt");
 require("dotenv").config();
 
 /**
+ *
+ * @param {string} nationalId 13 digit
+ * @param {string} amount
+ */
+function payToNationalId(nationalId, amount) {
+  const recipient = `0213${nationalId}`;
+  genPromptPay(recipient, amount, "nationalid");
+}
+
+/**
+ *
  * @param {string} phoneNumber
  * @param {string} amount
  */
 function payToPhone(phoneNumber, amount) {
   phoneNumber = phoneNumber.substr(1);
+  const recipient = `01130066${phoneNumber}`;
+  genPromptPay(recipient, amount, "phone");
+}
 
+/**
+ * @param {string} recipient 15 digit
+ * @param {string} amount
+ * @param {string} fileSuffix
+ */
+function genPromptPay(recipient, amount, fileSuffix) {
   // constant
   const a = "00020101021129370016";
   const b = "A000000677010111";
-  const promptPayCode = `01130066${phoneNumber}`;
   const c = "5802";
   const d = "TH54";
 
@@ -21,7 +40,7 @@ function payToPhone(phoneNumber, amount) {
   const f = "5303";
   const g = "764";
   const h = "6304";
-  const combine = `${a}${b}${promptPayCode}${c}${d}${e}${amount}${f}${g}${h}`;
+  const combine = `${a}${b}${recipient}${c}${d}${e}${amount}${f}${g}${h}`;
   const crcResult = crc16(combine).toString(16);
 
   // generate promptpay qr
@@ -29,12 +48,13 @@ function payToPhone(phoneNumber, amount) {
   console.log("result", payString);
   const encoder = new TextEncoder();
   const bytes = encoder.encode(payString);
-  QRCode.toFile(`output/promptpay-${phoneNumber}.png`, [
+  QRCode.toFile(`output/promptpay-${fileSuffix}.png`, [
     { data: new Uint8ClampedArray(bytes), mode: "byte" },
   ]);
 }
 
 // input
 const phoneNumber = process.env.PHONE_NUMBER;
-const amount = "3000.00";
+const amount = "2.00";
 payToPhone(phoneNumber, amount);
+payToNationalId(process.env.NATIONAL_ID, amount);
