386cd70eea4d48c8f1e05d2e132dc893babbb56c
Tanut-M2 <tanat44@gmail.com>
2026-04-27 19:25:28 +0200

    slow & fast loop control

--- arduino/esp32-gimbal-joystick/esp32-gimbal-joystick.ino
+++ arduino/esp32-gimbal-joystick/esp32-gimbal-joystick.ino
@@ -16,7 +16,7 @@ void setup() {
 
   joystick = new Joystick();
   joystick->begin(12, 13, 10, 11, 3, 2, 5, 4);
-  rgbLedWrite(LED_PIN, 0, 255, 0);
+  rgbLedWrite(LED_PIN, 255, 0, 0);
 
   bleGamepad.begin();
 
@@ -25,16 +25,37 @@ void setup() {
 
 
 void loop() {
-  joystick->tick();
-  joystick->printRaw();
+  static uint32_t lastTime1 = 0;
+  static uint32_t lastTime2 = 0;
 
+  // every 30 ms
+  if (millis() - lastTime1 > 30) {
+    lastTime1 = millis();
+    fastLoop();
+  } 
+
+  // every 1000ms
+  if (millis() - lastTime2 > 1000) {
+    lastTime2 = millis();
+    slowLoop();
+  }
+}
+
+void fastLoop() {
+  joystick->tick();
   if (bleGamepad.isConnected()) {
     int16_t values[4];
     joystick->getValues(values);
     bleGamepad.setAxes(values[0], values[1], values[2], values[3]);  //(X, Y, Z, RX, RY, RZ)
-    delay(1000);
+  } 
+}
+
+void slowLoop() {
+  joystick->printRaw();
+  if (bleGamepad.isConnected()) {
+    rgbLedWrite(LED_PIN, 0, 255, 0);
   } else {
+    rgbLedWrite(LED_PIN, 255, 0, 0);
     Serial.println("ble: not connected");
-    delay(1000);
   }
 }
