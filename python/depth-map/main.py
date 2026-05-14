5ac08b9fefe57f202413375b4363a56d65fcfc99
tanut-pi <tanat44@gmail.com>
2026-05-14 14:15:51 +0200

    show disparity

--- python/depth-map/main.py
+++ python/depth-map/main.py
@@ -5,10 +5,14 @@ from picamera2 import Picamera2
 from libcamera import controls, Transform
 import time
 
+
 def create_camera(id: int):
     cam = Picamera2(id)
-    cam.configure(cam.create_preview_configuration(main={"format": 'RGB888'}, transform=Transform(hflip=True)))
-    cam.set_controls({'AfMode': controls.AfModeEnum.Continuous})
+    cam.configure(cam.create_preview_configuration(
+        main={"format": 'RGB888'}, transform=Transform(hflip=True)))
+    # AfMode: 0=manual, 1=auto, 2=continuous
+    # LensPosition: 0-15 (0 is infinite, 15 is closest)
+    cam.set_controls({'AfMode': 0, 'LensPosition': 2})
     cam.start()
     return cam
 
@@ -18,28 +22,35 @@ camR = create_camera(0)
 y_shift = 0
 shift_matrix = np.float32([[1, 0, 0], [0, 1, y_shift]])
 stereo = cv2.StereoBM.create(numDisparities=16, blockSize=15)
+w = 640
+h = 480
 
 while True:
     # capture
-    frameL = camL.capture_array()
-    frameR = camR.capture_array()
-    frameR = cv2.warpAffine(frameR, shift_matrix, (frameR.shape[1], frameR.shape[0]))
+    frame_l = camL.capture_array()
+    frame_l = cv2.resize(frame_l, (w,h))
+    frame_r = camR.capture_array()
+    frame_r = cv2.resize(frame_r, (w,h))
+    frame_r = cv2.warpAffine(frame_r, shift_matrix,
+                            (frame_r.shape[1], frame_r.shape[0]))
 
     # disparity
-    frame_l_gray =  cv2.cvtColor(frameL, cv2.COLOR_BGR2GRAY)
-    frame_r_gray =  cv2.cvtColor(frameR, cv2.COLOR_BGR2GRAY)
-    disparity = stereo.compute(frame_l_gray,frame_r_gray).astype(np.float32)
-    
-    # display image
-    w = 640
-    h = 480
-    row0 = np.concatenate((cv2.resize(frameL, (w,h)), cv2.resize(frameR, (w,h))), axis=1)
-    row1 = np.concatenate((cv2.resize(frame_l_gray, (w,h)), cv2.resize(frame_r_gray, (w,h))), axis=1)
-    row1 = cv2.cvtColor(row1, cv2.COLOR_GRAY2BGR)
+    frame_l_gray = cv2.cvtColor(frame_l, cv2.COLOR_BGR2GRAY)
+    frame_r_gray = cv2.cvtColor(frame_r, cv2.COLOR_BGR2GRAY)
+    disparity = stereo.compute(frame_l_gray, frame_r_gray).astype(np.float32)
+    disparity = cv2.resize(disparity, (w, h))
 
-    output = np.concatenate((row0, row1), axis=0)
+    # display raw
+    row_0 = np.hstack((frame_l, frame_r))
+    row_1 = np.hstack((frame_l_gray, frame_r_gray))
+    row_1 = cv2.cvtColor(row_1, cv2.COLOR_GRAY2BGR)
+    output = np.vstack((row_0, row_1))
+
+    # display disparity
+    disparity_col = np.zeros((np.size(output,0), w, 3), np.uint8)
+    disparity_col[0:h,0:w] = cv2.cvtColor(disparity, cv2.COLOR_GRAY2BGR)
+    output = np.hstack((output, disparity_col))
     cv2.imshow('stereo camera', output)
 
     if cv2.waitKey(1) == ord('q'):
         break
- 
